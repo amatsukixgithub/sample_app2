bddb17ccca8e814b8e2d34014f8c1f29298cfaba
amatsukixprog <amatsukixprog@gmail.com>
2019-08-24 22:49:21 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    # debugger
+    @user = User.find(params[:id])
+  end
+
+  def create
+    # @user = User.new(params[:user])    # マスアサインメント　脆弱性あり
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+
+      # redirect_to user_url(@user) と同じ
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      # require(:user) user属性を必須とする
+      # permit() 指定された属性を許可する
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, size: 80)
+    #emailアドレスからgravatarのハッシュ化されたidを取得
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    #画像URL
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    #imageタグを返す
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
